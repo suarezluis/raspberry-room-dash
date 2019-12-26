ce75f1fb31ece797161394d614a0a5bd8a6e56fa
Luis Suarez <suarezluis@gmail.com>
2019-12-25 22:01:27 -0600

    heroku test

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -4,18 +4,25 @@ import "./Weather.css";
 
 class Weather extends Component {
   state = {
-    from: "8900 N Interstate 35, Austin TX, 78753",
-    to: "8834 N Capital of Texas Hwy, Austin, TX",
+    from: "8900 N Interstate 35 Austin TX 78753",
+    to: "8834 N Capital of Texas Hwy Austin TX",
+    randomFrom: "",
+    randomTo: "",
     timeToWork: "Unknown"
   };
 
   getTimeToWork = () => {
     console.log("requesting time to work");
+    console.log(this.state.randomFrom, this.state.randomTo);
     axios
-      .post("http://from-to-api.herokuapp.com/", {
-        from: this.state.from,
-        to: this.state.to
-      })
+      .post(
+        // "http://from-to-api.herokuapp.com/"
+        "http://localhost:8080",
+        {
+          from: this.state.from,
+          to: this.state.to
+        }
+      )
       .then(response => {
         console.log(response.data);
         this.setState({ timeToWork: response.data.time });
@@ -25,11 +32,28 @@ class Weather extends Component {
       });
   };
 
+  randomizeAddress = () => {
+    let randomA = " " + (Math.floor(Math.random() * 9999) + 1000);
+    let randomB = " " + (Math.floor(Math.random() * 9999) + 1000);
+    let splitFrom = this.state.from.split(",");
+    let splitTo = this.state.to.split(",");
+
+    this.setState({
+      randomFrom: splitFrom[0] + randomA + splitFrom[1],
+      randomTo: splitTo[0] + randomB + splitTo[1]
+    });
+  };
+
+  componentWillMount() {
+    this.randomizeAddress();
+  }
   componentDidMount() {
+    this.randomizeAddress();
     this.getTimeToWork();
     setInterval(() => {
+      this.randomizeAddress();
       this.getTimeToWork();
-    }, 15 * 60 * 1000);
+    }, 5 * 60 * 1000);
   }
 
   render() {
